2482d9fc27f7f3b729a5040440d0711f307eeba4
Soumik Sur <soumiksur.me@gmail.com>
2017-08-28 01:50:27 +0530

    1st section done

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import SearchBar from './search_bar'
 const API_KEY = 'AIzaSyBGlD1eGrWRk5KDsN7clyJDtcqsQFIn6IQ';
 
 export default class App extends Component {
+
   render() {
     return (
       <div><SearchBar /></div>
