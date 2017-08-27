0c8a4b76882d2048bdb9438c6fc1384112104fd9
Soumik Sur <soumiksur.me@gmail.com>
2017-08-27 13:43:56 +0530

    search componenet

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import SearchBar from './search_bar'
+const API_KEY = 'AIzaSyBGlD1eGrWRk5KDsN7clyJDtcqsQFIn6IQ';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div><SearchBar /></div>
     );
   }
 }
